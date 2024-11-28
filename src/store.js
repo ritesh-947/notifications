72984219ffc715cd09ff9c33345d01ef168dfd3a
ritesh-947 <ritesh.the.rk.star.123@gmail.com>
2024-11-28 13:35:48 +0530

    composeWithDevTools error solving

--- src/store.js
+++ src/store.js
@@ -1,22 +1,10 @@
-//auth2/authpage/src/store.js
-
-import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import {thunk} from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import rootReducer from './reducers';
 
-const initialState = {};
-
-const middleware = [thunk];
-
-const composeEnhancers = 
-  (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-
-const store = createStore(
-  rootReducer,
-  initialState,
-  composeEnhancers(applyMiddleware(...middleware))
-);
+const store = configureStore({
+  reducer: rootReducer,
+  enhancers: [composeWithDevTools()],
+});
 
 export default store;
